abace64f21b1befa1ac097bd341a5a362cb1d312
whaller-glitch <whaller69@gmail.com>
2026-08-03 00:37:56 -0400

    feat: make autotrader scraper return html

--- src/boxster_tracker/scrapers/autotrader.py
+++ src/boxster_tracker/scrapers/autotrader.py
@@ -19,20 +19,20 @@ class AutoTraderScraper(ListingScraper):
         """
 
         with sync_playwright() as p:
-
             browser = p.chromium.launch(
                 headless=True
             )
 
-            page = browser.new_page()
+            try:
+                page = browser.new_page()
 
-            page.goto(
-                url,
-                wait_until="networkidle",
-            )
+                page.goto(
+                    url,
+                    wait_until="networkidle",
+                    timeout=30000,
+                )
 
-            html = page.content()
+                return page.content()
 
-            browser.close()
-
-        return html
+            finally:
+                browser.close()
